91f15ae2b84e77c32a9562c51a6dba5d1ecf94dc
Luan Severo <luansevero@icloud.com>
2022-08-08 14:01:18 -0300

    [Feat] Buttons Style

--- src/components/shared/Button&Input.js
+++ src/components/shared/Button&Input.js
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 
-export default function Input(){
-
-    return(
-        <Inputs/>
-    )
-}
+const Buttons = styled.button`
+    padding: 20px 44px;
+    height: 60px;
+    background: #5D9040;
+    border-radius: 12px;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+`
 
 const Inputs = styled.input`
     width: 100%;
@@ -15,3 +18,5 @@ const Inputs = styled.input`
     box-shadow: 0px 4px 24px rgba(120, 177, 89, 0.12);
     border-radius: 12px;
 `
+
+export { Buttons, Inputs }
