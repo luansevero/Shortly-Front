84db0523a23c7e8764d2d2a7f9bd6f449ddee730
Luan Severo <luansevero@icloud.com>
2022-08-08 13:05:41 -0300

    Styles and main index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import './styled/reset.css'
+import './styled/globalStyle.css'
+import App from './App';
+
+ReactDOM.render(<App />, document.querySelector('.root'));
